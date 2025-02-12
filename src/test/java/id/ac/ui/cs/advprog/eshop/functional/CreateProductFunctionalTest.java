43b8750e81a83f87940f8be541afd3aaf328777d
ksmratih <ratihdun@gmail.com>
2025-02-12 20:39:57 +0700

    added CreateProductFunctionalTest.java

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,48 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = "http://localhost:" + serverPort;
+    }
+
+    @Test
+    void userCanCreateProduct(ChromeDriver driver) {
+        driver.get(baseUrl + "/product/create");
+
+        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        WebElement quantityInput = driver.findElement(By.id("quantityInput"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        nameInput.sendKeys("Test Product");
+        quantityInput.sendKeys("10");
+        submitButton.click();
+
+        driver.get(baseUrl + "/product/list");
+
+        WebElement productList = driver.findElement(By.tagName("body"));
+        assertTrue(productList.getText().contains("Test Product"));
+        assertTrue(productList.getText().contains("10"));
+    }
+}
