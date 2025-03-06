bd4bcd34cc1d34e55b48f5577b00c2570451f449
ksmratih <ratihdun@gmail.com>
2025-03-06 22:32:38 +0700

    [GREEN] Implement PaymentByVoucher model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
-
 import java.util.Map;
 
 public class PaymentByVoucher extends Payment {
@@ -16,8 +15,37 @@ public class PaymentByVoucher extends Payment {
 
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null || paymentData.isEmpty() || !paymentData.containsKey("voucherCode") || paymentData.get("voucherCode") == null) {
+            throw new IllegalArgumentException("Invalid voucher data");
+        }
+
+        String voucherCode = paymentData.get("voucherCode");
+        int digitCount = 0;
+
+        for (char c : voucherCode.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digitCount++;
+            }
+        }
+
+        super.setPaymentData(paymentData);
+
     }
 
     public void validateVoucher() {
+        String voucherCode = getPaymentData().get("voucherCode");
+        int digitCount = 0;
+
+        for (char c : voucherCode.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digitCount++;
+            }
+        }
+
+        if (voucherCode.length() == 16 && voucherCode.startsWith("ESHOP") && digitCount == 8) {
+            super.setStatus(PaymentStatus.SUCCESS.getValue());
+        } else {
+            super.setStatus(PaymentStatus.REJECTED.getValue());
+        }
     }
 }
